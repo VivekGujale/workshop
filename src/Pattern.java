54f30d0c7012153c11be25789af1ee35511d34d2
VivekGujale <vdgujale007@.com>
2022-12-23 09:15:12 +0530

    * pattern

--- src/Pattern.java
+++ src/Pattern.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Pattern {
     public static void main(String[] args) {
-        int i, j;
+        int i, j, row = 4;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of rows");
-        int row = scanner.nextInt();
         for (i = 1; i <= row; i++) {
             for (j = 1; j <= row; j++) {
                 System.out.print("*");
