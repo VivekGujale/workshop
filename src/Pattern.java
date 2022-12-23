e1d4119204263494c0da48136fbde820e020221e
VivekGujale <vdgujale007@.com>
2022-12-23 09:18:30 +0530

    * pattern program

--- src/Pattern.java
+++ src/Pattern.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Pattern {
     public static void main(String[] args) {
         int i, j, row = 4;
-        Scanner scanner = new Scanner(System.in);
         for (i = 1; i <= row; i++) {
             for (j = 1; j <= row; j++) {
                 System.out.print("*");
